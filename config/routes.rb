fc548d94aa0fab12aec79bf32463e6a4b6a565c1
tono0114 <tono.cookie_crunch@icloud.com>
2021-07-14 12:43:38 +0000

    [Add]確認用

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-    root_to:'homes#top'
-    resources :sessions, only: [:new, :create, :destroy]
+    devise_for :admins, path: "/", controllers: {
+      sessions: 'admin/sessions'
+    }
+    get 'homes/top' => 'homes#top'
     resources :items, except: [:destroy]
     resources :genres, except: [:new, :show, :destroy]
     resources :customers, except: [:new, :create, :destroy]
     resources :orders, only: [:show, :update]
     resources :order_items, only: [:update]
   end
-  
+
   namespace :public do
-    resources :homes, only: [:top, :about]
+    root to:'homes#top'
+    get 'homes/about' => 'homes#about'
     resources :items, only: [:index, :show]
     resources :registrations, only: [:new, :create]
     resources :sessions, only: [:new, :create, :destroy]
@@ -22,5 +23,5 @@ Rails.application.routes.draw do
     resources :orders, except: [:update, :destroy]
     resources :addresses, except: [:new, :show]
   end
-  
+
 end
